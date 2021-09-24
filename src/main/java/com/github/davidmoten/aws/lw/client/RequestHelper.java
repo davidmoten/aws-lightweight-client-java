6dab4e45cd123da98823a2ed34096b90928cf34b
David Moten <davidmoten@gmail.com>
2021-09-24 20:24:04 +1000

    security-token is expected in signature

--- src/main/java/com/github/davidmoten/aws/lw/client/RequestHelper.java
+++ src/main/java/com/github/davidmoten/aws/lw/client/RequestHelper.java
@@ -63,6 +63,8 @@ final class RequestHelper {
             h.put("x-amz-content-sha256", contentHashString);
         }
 
+        includeTokenIfPresent(credentials, h);
+
         List<Parameter> parameters = extractQueryParameters(endpointUrl);
         Map<String, String> q = parameters.stream()
                 .collect(Collectors.toMap(p -> p.name, p -> p.value));
