06a7b71f4b7552b3d528ae65a8886448ae0e7463
Dave Moten <davidmoten@gmail.com>
2022-01-11 21:14:40 +1100

    support no-region endpoints like iam.amazonaws.com (#40)
    * add support for regionless urls like iam.amazonaws.com
    
    * add regionNone method to builder
    
    * add unit test

--- src/test/java/com/github/davidmoten/aws/lw/client/ClientTest.java
+++ src/test/java/com/github/davidmoten/aws/lw/client/ClientTest.java
@@ -144,6 +144,28 @@ public class ClientTest {
         assertEquals(5000, hc.connectTimeoutMs);
         assertEquals(6000, hc.readTimeoutMs);
     }
+    
+    @Test
+    public void testRegionNoneUsesUsEast1InSignature() {
+        Client client = Client //
+                .iam() //
+                .regionNone()
+                .accessKey("123") //
+                .secretKey("456") //
+                .httpClient(hc) //
+                .build();
+        // create a bucket
+        client //
+                .query("Action", "GetUser") //
+                .query("Version", "2010-05-08") //
+                .execute();
+        assertEquals(
+                "https://iam.amazonaws.com/?Action=GetUser&Version=2010-05-08",
+                hc.endpointUrl.toString());
+        String authorization = hc.headers.get("Authorization");
+        assertTrue(authorization.contains("/us-east-1/iam/aws4_request"));
+        assertEquals("iam.amazonaws.com", hc.headers.get("Host"));
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void testBadConnectTimeout() {
@@ -225,7 +247,7 @@ public class ClientTest {
         map.put("AWS_ACCESS_KEY_ID", "123");
         map.put("AWS_SECRET_ACCESS_KEY", "abc");
         Client client = Client.s3().environment(name -> map.get(name)).defaultClient().build();
-        assertEquals("ap-southeast-2", client.region());
+        assertEquals("ap-southeast-2", client.region().get());
         Credentials c = client.credentials();
         assertEquals("123", c.accessKey());
         assertEquals("abc", c.secretKey());
@@ -238,7 +260,7 @@ public class ClientTest {
         System.setProperty("aws.secretKey", "abc");
         Client client = Client.s3().region("ap-southeast-2").credentialsFromSystemProperties()
                 .build();
-        assertEquals("ap-southeast-2", client.region());
+        assertEquals("ap-southeast-2", client.region().get());
         Credentials c = client.credentials();
         assertEquals("123", c.accessKey());
         assertEquals("abc", c.secretKey());
