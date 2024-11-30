cd2ab698ba419bbb5505524593166ced3206d188
Dave Moten <davidmoten@gmail.com>
2024-11-30 17:48:15 +1100

    dev

--- src/main/java/com/github/davidmoten/aws/lw/client/internal/EnvironmentHelper.java
+++ src/main/java/com/github/davidmoten/aws/lw/client/internal/EnvironmentHelper.java
@@ -1,8 +1,6 @@
 package com.github.davidmoten.aws.lw.client.internal;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
