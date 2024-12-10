4546c87917b19dfde8af75d2b47f6f083217bbed
Dave Moten <davidmoten@gmail.com>
2024-12-10 18:47:32 +1100

    SnapStart support #191 (#194)

--- src/main/java/com/github/davidmoten/aws/lw/client/internal/Environment.java
+++ src/main/java/com/github/davidmoten/aws/lw/client/internal/Environment.java
@@ -1,8 +1,7 @@
 package com.github.davidmoten.aws.lw.client.internal;
 
-import java.util.Optional;
-
 import com.github.davidmoten.aws.lw.client.Credentials;
+import com.github.davidmoten.aws.lw.client.HttpClient;
 
 @FunctionalInterface
 public interface Environment {
@@ -10,8 +9,7 @@ public interface Environment {
     String get(String name);
 
     default Credentials credentials() {
-        return new CredentialsImpl(get("AWS_ACCESS_KEY_ID"), get("AWS_SECRET_ACCESS_KEY"),
-                Optional.ofNullable(get("AWS_SESSION_TOKEN")));
+        return CredentialsHelper.credentialsFromEnvironment(this, HttpClient.defaultClient());
     }
 
     static Environment instance() {
